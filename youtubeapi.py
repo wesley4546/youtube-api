7b63dcdd76b96e8764df086453dd8fe88f9b5a72
wesley4546 <wesley.gardiner4546@gmail.com>
2020-06-02 00:07:46 -0400

    Added youtube class and beginning of loop

--- youtubeapi.py
+++ youtubeapi.py
@@ -5,8 +5,54 @@ from source.get_video_information import get_video_information
 from source.buildinfo import buildinfo  # This is just a python file with APIKEY as a variable
 from source.get_video_transcripts import get_video_transcripts
 
-video_ids = get_video_ids("starcraft", 10, buildinfo)
 
-video_information = get_video_information(video_ids, buildinfo)
+class youtubevideo:
+    """
+    Class for the videos to be stored in
+    """
 
-video_transcripts = get_video_transcripts(video_ids[0])
+    def __init__(self, id, title, description, views, published, likes, dislikes,
+                 comment_count, channel_name, channel_id, transcription):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.views = views
+        self.published = published
+        self.likes = likes
+        self.dislikes = dislikes
+        self.comment_count = comment_count
+        self.channel_name = channel_name
+        self.channel_url = channel_id
+        self.transcription = transcription
+
+
+video_ids = get_video_ids('starcraft', 10, buildinfo)
+
+
+
+
+
+
+
+
+
+
+for id in video_ids:
+
+    video_information = get_video_information(id, buildinfo)
+
+    video_transcripts = get_video_transcripts(video_ids[id])
+
+    yt_v = youtubevideo(
+        id=video_information[0]['id'],
+        title=video_information[0]['snippet']['title'],
+        description=video_information[0]['snippet']['description'],
+        views=video_information[0]['statistics']['viewCount'],
+        published=video_information[0]['snippet']['publishedAt'],
+        likes=video_information[0]['statistics']['likeCount'],
+        dislikes=video_information[0]['statistics']['dislikeCount'],
+        comment_count=video_information[0]['statistics']['commentCount'],
+        channel_name=video_information[0]['snippet']['channelTitle'],
+        channel_id= video_information[0]['snippet']['channelId'],
+        transcription=video_transcripts
+    )
